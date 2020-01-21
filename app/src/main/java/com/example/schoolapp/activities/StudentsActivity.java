ef5b842ddb6cc4e8fc9dd9d21abd8995d647d744
jurke95 <markivicius@hotmail.com>
2020-01-21 16:43:25 +0100

    back button

--- app/src/main/java/com/example/schoolapp/activities/StudentsActivity.java
+++ app/src/main/java/com/example/schoolapp/activities/StudentsActivity.java
@@ -1,23 +1,42 @@
 package com.example.schoolapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.schoolapp.R;
 import com.example.schoolapp.adapters.MyAdapter;
 import com.example.schoolapp.adapters.RecyclerViewAdapter;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class StudentsActivity extends AppCompatActivity {
+public class StudentsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_students);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ArrayList<String> students = new ArrayList<>();
         ArrayList<String> imagesUrl = new ArrayList<>();
@@ -47,6 +66,54 @@ public class StudentsActivity extends AppCompatActivity {
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, students, imagesUrl);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.settings:
+                Intent s = new Intent(this, SettingsActivity.class);
+                startActivity(s);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch(menuItem.getItemId()){
+
+            case R.id.nav_class:
+                drawer.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.nav_subjects:
+                Intent s = new Intent(this, SubjectsActivity.class);
+                startActivity(s);
+                break;
+            case R.id.nav_announcements:
+                Intent a = new Intent(this, MainActivity.class);
+                startActivity(a);
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
