60a5039197d90386927912b3bcc8fa1c3eb90ee0
aleksandragr <grujica995@gmail.com>
2020-02-04 01:14:52 +0100

    some fixes

--- app/src/main/java/com/example/schoolapp/Config/MyApplication.java
+++ app/src/main/java/com/example/schoolapp/Config/MyApplication.java
@@ -5,7 +5,7 @@ import android.app.Application;
 public class MyApplication extends Application {
 
 
-    private static String serverIpAddress = "http://192.168.0.16:5000/";
+    private static String serverIpAddress = "http://192.168.0.15:5000/";
 
 
     public static String getServerIpAddress(){
